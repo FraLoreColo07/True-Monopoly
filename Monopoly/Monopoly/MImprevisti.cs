ff5106374929477968a6116a47e137c5bf34aa6b
FraLoreColo07 <57748531+FraLoreColo07@users.noreply.github.com>
2020-03-08 13:34:40 +0100

    Aggiornamento classi

--- Monopoly/Monopoly/MImprevisti.cs
+++ Monopoly/Monopoly/MImprevisti.cs
@@ -9,11 +9,30 @@ namespace Monopoly
     class MImprevisti
     {
         private List<MElementi> Imprevisti = null;
+        private int countElement = 0;
 
         public MImprevisti() { }
         public void CreaListaImprevisti(string fileName)
         {
             FileXML.deserializza(out Imprevisti, "Imprevisti.xml");
+            this.countElement = Imprevisti.Count;
+        }
+        private void setImprevisti()
+        {
+            Random rand = new Random();
+
+            for (int i = 0, num = 0; i < Imprevisti.Count; i++)
+            {
+                num = rand.Next(countElement);
+                swap(ref Imprevisti[i],ref Imprevisti[num]);
+            }
+        }
+
+        private void swap(ref MElementi e1, ref MElementi e2)
+        {
+            MElementi temp = e1;
+            e1 = e2;
+            e2 = temp;
         }
 
     }
